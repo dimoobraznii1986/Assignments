cfbb7b7895366140deb19ebfad824dd4b9afbf23
Dmitry <98490741+dimoobraznii1986@users.noreply.github.com>
2022-05-29 15:30:25 -0700

    Update test_clockwise.py

--- test_clockwise.py
+++ test_clockwise.py
@@ -1,8 +1,5 @@
 from clockwise.clockwise import clockwiseMatrix
 
-TestMatrix1 = [[1,2],
-              [3,4]]
- 
-def test_clockwise(TestMatrix1):
-    result = clockwiseMatrix(TestMatrix1)
+def test_clockwise():
+    result = clockwiseMatrix([[1,2],[3,4]])
     assert "1 2 3 4" == result
