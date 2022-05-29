568bc2d272236f450777c06d886914f5afddc72b
Dmitry <98490741+dimoobraznii1986@users.noreply.github.com>
2022-05-29 15:21:18 -0700

    Update test-clockwise.py

--- test-clockwise.py
+++ test-clockwise.py
@@ -4,5 +4,5 @@ TestMatricx = [[1,2],
                [3,4]]
  
 def test_clockwise(TestMatrix):
-  result = clockwiseMatrix(TestMatrix)
-  assert "1 2 3 4" == result
+    result = clockwiseMatrix(TestMatrix)
+    assert "1 2 3 4" == result
