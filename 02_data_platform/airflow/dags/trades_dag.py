53d92dd6358d62664b69758002c9f063f80012f5
Dmitry Anoshin <98490741+dimoobraznii1986@users.noreply.github.com>
2023-11-13 21:51:34 -0800

    Docker Updates

--- 02_data_platform/airflow/dags/trades_dag.py
+++ 02_data_platform/airflow/dags/trades_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -16,9 +16,13 @@ dag = DAG(
     'trades_dag', default_args=default_args, schedule_interval=timedelta(days=1)
 )
 
-run_dbt = BashOperator(
+run_dbt = DockerOperator(
     task_id='run_dbt',
-    bash_command='dbt run --select fct_pdt_count --profiles-dir . --project-dir dbt_trades',
+    image='02_data_platform-dbt-1', 
+    api_version='auto',
+    auto_remove=True,
+    command='dbt run --select fct_pdt_count --profiles-dir . --project-dir /path/to/dbt_trades', 
+    network_mode='tradesnetwork', 
     dag=dag,
 )
 
